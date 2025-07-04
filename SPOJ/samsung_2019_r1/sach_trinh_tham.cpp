e54de50c75dc6e3e5e41820f0cdb80067289d1b6
unknown <vuthanhnguyen2005@gmail.com>
2025-07-04 19:02:35 +0700

    VuThanhNguyen

---
+++ SPOJ/samsung_2019_r1/sach_trinh_tham.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define FOR(i,a,b) for (int i = (a); i <= (b); i++)
+#define FOD(i,a,b) for (int i = (a); i >= (b); i--)
+//#define int long long
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define sz size
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define ii pair<int,int>
+#define fi first
+#define sc second
+#define faster ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0)
+
+const ll MOD = 1e9 + 7;
+const int MAXN = 1e5 + 5;
+const double EPS = 1e-10;
+const int INF = 1e9;
+
+using namespace std;
+
+signed main(){
+    faster;
+    int n;
+    cin >> n;
+    vi a(n + 1);
+    for(int i = 1; i <= n; i++) {
+        cin >> a[i];
+    }
+    int i = 1;
+    int mx = a[i];
+    int ans = 0;
+    while(i <= n) {
+        mx = a[i];
+        int j = i;
+        while(j < mx) {
+            j++;
+            mx = max(mx, a[j]);
+            
+        }
+        ans++;
+        i = j + 1;
+    }
+    cout << ans;
+}
