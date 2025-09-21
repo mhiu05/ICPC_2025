b6725dee0ca69a0327d10b25fa3dd5ec23fe771a
mhiu05 <minhhiuhh2k5@gmail.com>
2025-09-21 07:05:30 +0700

    update

--- Contest_ICPC_2025/Contest_5/H.cpp
+++ Contest_ICPC_2025/Contest_5/H.cpp
@@ -1,58 +1,31 @@
 #include <bits/stdc++.h>
+#define ll long long
+#define int long long
+#define FOR(i,a,b) for (int i = (a); i <= (b); i++)
+#define FOD(i,a,b) for (int i = (a); i >= (b); i--)
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define pf push_front
+#define pb push_back
+#define sz size
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define ii pair<int,int>
+#define fi first 
+#define sc second
+#define faster ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0)
+
+const ll MOD = 1e9 + 7;
+const int MAXN = 1e5 + 5;
+const double EPS = 1e-10;
+const int INF = 1e9;
+
 using namespace std;
 
-using ll = long long;
+signed main(){
+    faster;
 
-const int INF = 1e9 + 7;
-const int maxN = 1005;
-
-int n, m, r, c;
-int board[maxN][maxN];
-int pre[maxN][maxN];
-
-bool isSuccess(int median) {
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= m; j++) {
-            int current = (board[i][j] <= median) ? 1 : -1;
-            pre[i][j] = current + pre[i][j-1] + pre[i-1][j] - pre[i-1][j-1];
-        }
-    }
-
-    for (int i = r; i <= n; i++) {
-        for (int j = c; j <= m; j++) {
-            int sum = pre[i][j] - pre[i-r][j] - pre[i][j-c] + pre[i-r][j-c];
-            if (sum > 0) { // số phần tử <= medium lớn hơn số phần tử > medium
-                return true; // => số cần tìm (truth medium) <= medium
-            }
-        }
-    }
-    return false;
-}
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-
-    cin >> n >> m >> r >> c;
-
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= m; j++) {
-            cin >> board[i][j];
-        }
-    }
-
-    int left = 0, right = INF;
-    while (right - left > 1) {
-        int mid = (left + right) / 2;
-        if (isSuccess(mid)) {
-            right = mid;
-        } else {
-            left = mid;
-        }
-    }
-
-    cout << right << endl;
+    
 
     return 0;
 }
